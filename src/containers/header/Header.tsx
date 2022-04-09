aade0b9737e5ceb243dda0b8123673c7f96388e4
HARSH SHARMA <60179664+HRSRaiden@users.noreply.github.com>
2022-04-09 11:13:49 +0530

    people.png

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './header.css';
-
+import people from '../../assets/people.png';
+// import people from '../../assets/people.png';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='cu__header section__padding' id='home'>
+      <div className='cu__header-content'>
+        <h1 className='gradient__text'>Welcome to CU, a community to learn, grow and have fun together.</h1>
+        <p>We are currently looking for people to share their public meeting experiences with us in photographs and description to build a community. These contents will be open source and will help build our community. Community partners will get perks like free premium membership for 4 months.</p>
+        <div className='cu__header-content__input'>
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started</button  >
+        </div>
+        <div className='cu__header-content__people'>
+          <img src={people} />          
+
+        </div>
+      </div>
+      </div>
   )
 }
 
